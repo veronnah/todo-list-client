d8ce02dd71f3ba3bf9e946e20958036eae9bdf74
veronnah <yoo.veroon@gmail.com>
2021-11-06 18:52:49 +0100

    first setup, layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
+import { TaskFormComponent } from './task-form/task-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    TaskFormComponent
   ],
   imports: [
     BrowserModule,
